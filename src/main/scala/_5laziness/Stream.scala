ec2d66c3dfa756271cf5200bf552500b30acee46
Ilja <ikempf@xebia.fr>
2015-11-30 16:39:16 +0100

    [chapter 5]: Laziness

--- src/main/scala/_5laziness/Stream.scala
+++ src/main/scala/_5laziness/Stream.scala
@@ -1,10 +1,13 @@
 package _5laziness
 
-import _5laziness.Stream.cons
+import _5laziness.Stream.{cons, empty, unfold}
 
 import scala.annotation.tailrec
 
 trait Stream[+A] {
+
+  def exists(p: A => Boolean): Boolean =
+    foldRight(false)((a, b) => p(a) || b)
 
   def toList: List[A] = this match {
     case Cons(h, t) => h() :: t().toList
@@ -49,8 +52,86 @@ trait Stream[+A] {
   }
 
   def takeWhileViaFoldRight(f: A => Boolean): Stream[A] =
-    foldRight(Empty:Stream[A])((a, z) => if (f(a)) cons(a, z) else Empty)
+    foldRight(empty[A])((a, z) => if (f(a)) cons(a, z) else Empty)
 
+  def headOption: Option[A] = this match {
+    case Cons(h, t) => Some(h())
+    case _ => None
+  }
+
+  def headOptionViaFoldRight: Option[A] =
+    foldRight(None: Option[A])((a, _) => Some(a))
+
+  def map[B](f: A => B): Stream[B] =
+    foldRight(empty[B])((a, z) => cons(f(a), z))
+
+  def filter(f: A => Boolean): Stream[A] =
+    foldRight(empty[A])((a, z) => if (f(a)) cons(a, z) else z)
+
+  def append[B >: A](stream2: Stream[B]): Stream[B] =
+    foldRight(stream2)(cons(_, _))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] =
+    foldRight(empty[B])((a, z) => f(a).append(z))
+
+  def mapViaUnfold[B](f: A => B): Stream[B] =
+    unfold(this) {
+      case Cons(h, t) => Some(f(h()), t())
+      case Empty => None
+    }
+
+  def takeViaUnfold(n: Int): Stream[A] =
+    unfold((this, n)) {
+      case (Cons(h, t), m) if m > 0 => Some(h(), (t(), m - 1))
+      case _ => None
+    }
+
+  def takeWhileViaUnfold(f: A => Boolean): Stream[A] =
+    unfold(this) {
+      case Cons(h, t) if f(h()) => Some(h(), t())
+      case _ => None
+    }
+
+  def zipWith[B, C](stream: Stream[B])(f: (A, B) => C): Stream[C] =
+    unfold((this, stream)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some(f(h1(), h2()), (t1(), t2()))
+      case _ => None
+    }
+
+  def zipAll[B, C](stream: Stream[B]): Stream[(Option[A], Option[B])] =
+    unfold((this, stream)) {
+      case (Cons(h1, t1), Cons(h2, t2)) => Some((Some(h1()), Some(h2())), (t1(), t2()))
+      case (Cons(h1, t1), _) => Some((Some(h1()), None), (t1(), Empty))
+      case (_, Cons(h2, t2)) => Some((None, Some(h2())), (Empty, t2()))
+      case _ => None
+    }
+
+  def startsWith[B >: A](stream: Stream[B]): Boolean =
+    zipAll(stream).takeWhile(_._2.isDefined).forAll(h => h._1 == h._2)
+
+  def tails: Stream[Stream[A]] = this match {
+    case Cons(_, t) => cons(this, t().tails)
+    case Empty => Stream(Empty)
+  }
+
+  def hasSubsequence[B](s: Stream[B]): Boolean =
+    tails exists (_ startsWith s)
+
+  // !TODO: get this to work ...
+  def scanRight[B](z: B)(f: (A, => B) => B): Stream[B] = {
+    def scanInner(s: Stream[A], z2:B, y: Stream[B]): Stream[B] = s match {
+      case Empty => y
+      case Cons(h, t) => scanInner(t(), f(h(), z2), cons(f(h(), z2), y))
+    }
+
+    scanInner(this, z, Stream(z))
+  }
+
+  def scanRightViaFoldRight[B](z: B)(f: (A, => B) => B): Stream[B] =
+    foldRight((z, Stream(z)))((a, acc) => {
+      val b = f(a, acc._1)
+      (b, cons(b, acc._2))
+    })._2
 }
 
 case object Empty extends Stream[Nothing]
@@ -69,5 +150,35 @@ object Stream {
   def apply[A](as: A*): Stream[A] =
     if (as.isEmpty) empty
     else cons(as.head, apply(as.tail: _*))
+
+  val ones: Stream[Int] = cons(1, ones)
+
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
+
+  def from(n: Int): Stream[Int] =
+    cons(n, from(n + 1))
+
+  def fib: Stream[Int] = {
+    def fibInner(f1: Int, f2: Int): Stream[Int] = cons(f1, fibInner(f2, f1 + f2))
+
+    fibInner(0, 1)
+  }
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case Some((a, s)) => cons(a, unfold(s)(f))
+    case None => Empty
+  }
+
+  def onesViaUnfold: Stream[Int] = unfold(1)(_ => Some(1, 1))
+
+  def constantViaUnfold[A](a: A): Stream[A] = unfold(a)(_ => Some(a, a))
+
+  def fromViaUnfold(a: Int): Stream[Int] = unfold(a)(s => Some(s, s + 1))
+
+  def fibViaUnfold: Stream[Int] =
+    unfold((0, 1)) { case (f1, f2) => Some(f1, (f2, f1 + f2)) }
+
+  def asList[A](stream: Stream[Stream[A]]): List[List[A]] =
+    stream.map((a) => a.toList).toList
 
 }
--- src/test/scala/_5laziness/StreamTest.scala
+++ src/test/scala/_5laziness/StreamTest.scala
@@ -41,5 +41,109 @@ class StreamTest extends FlatSpec with Matchers {
     Stream(5, 6, 7, 8).takeWhileViaFoldRight(_ < 3).toList should equal(List())
   }
 
+  "HeadOption" should "return the head of non empty streams" in {
+    Stream(1, 4, 6, 7).headOption should equal(Some(1))
+    Stream().headOption should equal(None)
+  }
+
+  "HeadOptionViaFoldRight" should "return the head of non empty streams" in {
+    Stream(1, 4, 6, 7).headOptionViaFoldRight should equal(Some(1))
+    Stream().headOptionViaFoldRight should equal(None)
+  }
+
+  "Map" should "apply given function to values of stream" in {
+    Stream(1, 3, 9).map(_ * 5).toList should equal(List(5, 15, 45))
+  }
+
+  "Filter" should "filter elements not satisfying given predicate" in {
+    Stream().filter((_) => true).toList should equal(List())
+    Stream(4, 7, 8).filter(_ % 4 != 0).toList should equal(List(7))
+  }
+
+  "Append" should "append two stream" in {
+    Stream(2, 6).append(Stream(3, 9)).toList should equal(List(2, 6, 3, 9))
+  }
+
+  "FlatMap" should "apply given function to elements of the stream" in {
+    Stream(5, 10, 13).flatMap(Stream(_)).toList should equal(List(5, 10, 13))
+  }
+
+  "Constant" should "generate an infinite stream from a given value" in {
+    Stream.constant("foo").take(3).toList should equal(List("foo", "foo", "foo"))
+    Stream.constant(10).take(2).toList should equal(List(10, 10))
+  }
+
+  "From" should "generate an infinite icremental stream starting from the given value" in {
+    Stream.from(10).take(4).toList should equal(List(10, 11, 12, 13))
+  }
+
+  "Fib" should "generate the fibonacci sequence" in {
+    Stream.fib.take(10).toList should equal(List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+  }
+
+  "OnesViaUnfold" should "generate an infinite stream of ones" in {
+    Stream.onesViaUnfold.take(4).toList should equal(List(1, 1, 1, 1))
+  }
+
+  "ConstantViaUnfold" should "generate an infinite stream from a given value" in {
+    Stream.constantViaUnfold("foo").take(3).toList should equal(List("foo", "foo", "foo"))
+    Stream.constantViaUnfold(10).take(2).toList should equal(List(10, 10))
+  }
+
+  "FromViaUnfold" should "generate an infinite icremental stream starting from the given value" in {
+    Stream.fromViaUnfold(10).take(4).toList should equal(List(10, 11, 12, 13))
+  }
+
+  "FibViaUnfold" should "generate the fibonacci sequence" in {
+    Stream.fibViaUnfold.take(10).toList should equal(List(0, 1, 1, 2, 3, 5, 8, 13, 21, 34))
+  }
+
+  "MapViaUnfold" should "apply given function to values of stream" in {
+    Stream(1, 3, 9).mapViaUnfold(_ * 5).toList should equal(List(5, 15, 45))
+  }
+
+  "TakeViaUnfold" should "take n first elemnts of the stream" in {
+    Stream(1).takeViaUnfold(2).toList should equal(List(1))
+    Stream(1, 2, 3).takeViaUnfold(2).toList should equal(List(1, 2))
+  }
+
+  "TakeWhileViaUnfold" should "take starting elements respecting the given predicate" in {
+    Stream(5, 6, 7, 8).takeWhileViaUnfold(_ <= 7).toList should equal(List(5, 6, 7))
+    Stream(5, 6, 7, 8).takeWhileViaUnfold(_ < 3).toList should equal(List())
+  }
+
+  "ZipWith" should "combine elements of both streams" in {
+    Stream(1, 2, 3).zipWith(Stream(4, 5, 6))(_ + _).toList should equal(List(5, 7, 9))
+    Stream(1, 2).zipWith(Stream(4, 5, 6))(_ + _).toList should equal(List(5, 7))
+    Stream(1, 2, 3, 4).zipWith(Stream(4, 5))(_ + _).toList should equal(List(5, 7))
+  }
+
+  "ZipAll" should "combine elements of both streams" in {
+    Stream(1, 2).zipAll(Stream(4, 5)).toList should equal(List((Some(1), Some(4)), (Some(2), Some(5))))
+    Stream(1, 2, 3).zipAll(Stream(4, 5)).toList should equal(List((Some(1), Some(4)), (Some(2), Some(5)), (Some(3), None)))
+    Stream(1, 2).zipAll(Stream(4, 5, 6)).toList should equal(List((Some(1), Some(4)), (Some(2), Some(5)), (None, Some(6))))
+  }
+
+  "StartsWith" should "chech if stream starts with given stream" in {
+    Stream(1, 2, 3, 4).startsWith(Stream(1, 2, 3)) should be(true)
+    Stream(1, 2, 3, 4).startsWith(Stream(1, 3)) should be(false)
+  }
+
+  "Tails" should "return all suffixes of the stream" in {
+    Stream.asList(Stream(1, 2, 3).tails) should equal(List(List(1, 2, 3), List(2, 3), List(3), List()))
+  }
+
+  "HasSubsequence" should "be true if contains given sequence" in {
+    Stream(4, 5, 6, 7, 10, 13).hasSubsequence(Stream(6, 7, 10)) should be(true)
+  }
+
+  //!TODO
+  //  "ScanRight" should "scan the stream from right to left" in {
+  //    Stream(1, 2, 3).scanRight(4)(_ + _).toList should equal(List(10, 9, 7, 4))
+  //  }
+
+  "ScanRightViaFoldRight" should "scan the stream from right to left" in {
+    Stream(1, 2, 3).scanRightViaFoldRight(4)(_ + _).toList should equal(List(10, 9, 7, 4))
+  }
 
 }
